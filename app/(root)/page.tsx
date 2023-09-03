ae127ac6e8759e7816f307c3e11bcfd0d2f80035
riqthedev <riqthedev@gmail.com>
2023-09-03 00:06:18 -0400

    building feed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,32 @@
-import { UserButton } from "@clerk/nextjs";
+'use server'
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
+
+export default async function Home() {
+  const result = await fetchPosts(1, 30);
+  const user = await currentUser()
+
  
-export default function Home() {
+
   return (
-    <div>
+    <>
       <h1 className="head-text text-left"> Home </h1>
-    </div>
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result"> No threads founds</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard 
+              key={post._id}
+              id={post._id}
+              currentUserId={user.id}
+              />
+            ))}
+          </>
+        )}
+
+      </section>
+    </>
   )
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,7 +1,42 @@
+
+
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model"
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
+
+
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+        .sort({ createdAt: 'desc' })
+        .skip(skipAmount)
+        .populate({ path: 'author', model: User })
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: '_id name parentId image'
+            }
+        })
+    const totalPostsCount = await Thread.countDocuments({
+        parentId: { $in: [null, undefined] },
+    }); // Get the total count of posts
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext }
+}
+
+
+
 
 interface Params {
     text: string,
@@ -24,3 +59,18 @@ export async function createThread({ text, author, communityId, path }: Params) 
     })
     revalidatePath(path)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
